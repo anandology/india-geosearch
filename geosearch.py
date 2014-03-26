ac59dcee687d15055b4e43b295b023e5644d0e30
Anand Chitipothu <anandology@gmail.com>
2014-03-26 15:12:49 +0530

    first version.

---
+++ geosearch.py
@@ -0,0 +1,23 @@
+import web
+import json
+
+urls = (
+    "/geosearch", "geosearch"
+)
+app = web.application(urls, globals())
+db = web.database(dbn="postgres", db="geosearch")
+
+class geosearch:
+    def GET(self):
+        i = web.input(lat=None, lon=None)
+        if not i.lat or i.lon:
+            return "Please specify lat and lon parameters"
+
+        point = 'Point({0} {1})'.format(i.lat, i.lon)
+        q = "SELECT pc_no, pc_name FROM pc WHERE ST_Within(ST_GeomFromText($point, 4326), geom)"
+        result = db.query(q, vars={"point": point})
+        match = result and result[0] or None
+        return json.dumps(match)
+
+if __name__ == "__main__":
+    app.run()
