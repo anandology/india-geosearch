d5a4e1dd22153921450b9699f9219cdb073b0198
Anand Chitipothu <anandology@gmail.com>
2014-04-06 00:35:30 +0530

    added jquery plugin and an example.

---
+++ js/jquery-geosearch.js
@@ -0,0 +1,26 @@
+(function($) {
+    $.fn.geosearch = function(options) {
+      this.each(function() {
+        var autocomplete = new google.maps.places.Autocomplete(this);
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+            if (!place.geometry)
+                return;
+            var url = "http://geosearch-anandology.rhcloud.com/geosearch?lat=" + place.geometry.location.A + "&lon=" + place.geometry.location.k;
+            console.log(place.geometry.location);
+            $.getJSON(url, function(response) {
+              if (response) {
+                if (options.success)
+                  options.success(response);
+              }
+              else {
+                if (options.error) 
+                  options.error();
+              }
+              console.log(response);
+            });
+        });
+      });
+      //
+    }
+}(jQuery));
